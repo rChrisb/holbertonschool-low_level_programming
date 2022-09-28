ac5a020a8eeb373d3d38fb115edd569e70c98aba
rChrisb <5767@holbertonstudents.com>
2022-09-28 04:47:26 -0700

    uses printf instead of puts

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Using the function printf
+ * instead of the function puts'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
