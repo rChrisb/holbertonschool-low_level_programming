83993520b5bb89e5f97a9ff4ba6224f3dd99de4e
rChrisb <5767@holbertonstudents.com>
2022-09-28 03:53:58 -0700

    My first C programm!

---
+++ hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program displays
+ * 'Programming is like building a multilingual puzzle'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
+
