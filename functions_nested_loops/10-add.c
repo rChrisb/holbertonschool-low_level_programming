4f7f39d4186ba411346ac3b0d6ecfb7920673c16
rChrisb <5767@holbertonstudents.com>
2022-10-04 07:09:18 -0700

    adds two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers and return value
+ * @n: first parameter
+ * @c: second parameter
+ * Return: return the addition
+ *
+ */
+
+int add(int n, int c)
+{
+	int r = n + c;
+
+	_putchar((r) + '0')
+	return (r);
+}
