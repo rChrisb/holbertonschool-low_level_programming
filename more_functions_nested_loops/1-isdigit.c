9d4a54239b6cc5a6b253cf8901d5b7aee1d83ad7
rChrisb <5767@holbertonstudents.com>
2022-10-05 07:26:35 -0700

    prints if number is a digit between 0 and 9

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * _isdigit - function checking if number is a digit 0-9
+ * @c: function parameter
+ *
+ * Description: single letter input
+ *
+ * Return: 0 (Success) or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
