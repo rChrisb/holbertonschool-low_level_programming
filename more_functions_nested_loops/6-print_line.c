9c965e8d0842b9fa98840a687d8b1da2a4271982
rChrisb <5767@holbertonstudents.com>
2022-10-06 03:19:15 -0700

    prints a line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * print_line - prints a straight line
+ * @n: parameter
+ *
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (line = 0; line <= n; line++)
+	{
+		_putchar('_');
+	}
+}
+
