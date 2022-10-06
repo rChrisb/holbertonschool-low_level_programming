e4eaa4468e3d965edab33beb15409fc27a1f8176
rChrisb <5767@holbertonstudents.com>
2022-10-06 07:29:05 -0700

    fizz buzz game

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry
+ *
+ * Return: always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 !=0))
+			printf("%s ", f);
+		else if ((i % 5 == 0) && (i % 3 != 0))
+			printf("%s ", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
+		else if (i == 100)
+			printf("%s", b);
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
