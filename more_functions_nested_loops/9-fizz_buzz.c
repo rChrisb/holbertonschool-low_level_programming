0054530fba9e1ff70642782e7618cb5f56eb8e74
rChrisb <5767@holbertonstudents.com>
2022-10-06 07:44:56 -0700

    fizzbuzz game

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -19,14 +19,14 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 != 0))
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 != 0))
 			printf("%s ", f);
 		else if ((i % 5 == 0) && (i % 3 != 0))
 			printf("%s ", b);
 		else if ((i % 3 == 0) && (i % 5 == 0))
 			printf("%s ", fb);
-		else if (i == 100)
-			printf("%s", b);
 		else
 		{
 			printf("%d ", i);
