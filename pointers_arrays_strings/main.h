ab14d30e4565cc26281de15ab314d1e08292019c
rChrisb <5767@holbertonstudents.com>
2022-10-10 01:12:24 -0700

    a function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ *
+ * Structs, enums and unions defintions
+ * Typedefs
+ * Function prototypes
+ *
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif
