88a2eaae1d3eb13d8fba6507bd8fb6e23a6984ef
rChrisb <5767@holbertonstudents.com>
2022-10-10 04:48:09 -0700

    prints revered string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,19 @@
 
 void print_rev(char *s)
 {
-	int i;
 	int count = 0;
 
 	for (; *s != '\0'; s++)
 	{
 		count++;
-		for (i = count - 1; i >= 0; i--)
-		{
-			_putchar(*(s + i));
-		}
 	}
+	
+	int i;
+	
+	for (i = count - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	
 	_putchar('\n');
 }
