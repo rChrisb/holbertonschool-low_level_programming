01862192d4618e8e6501b3ebc2509fc503ab4a1f
rChrisb <5767@holbertonstudents.com>
2022-10-11 09:31:35 -0700

    copies n bytes from memory area src to memory area dest

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memcpy
+ * @dest: 1st param
+ * @src: 2nd param
+ * @n: 3rd param
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *d = dest;
+	const char *s = src;
+
+	while (n--)
+		*d++ = *s++;
+
+	return (dest);
+}
