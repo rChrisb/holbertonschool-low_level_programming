c291906b60fd35bb33de8c87d79c884b9c2665bc
rChrisb <5767@holbertonstudents.com>
2022-10-16 02:00:26 -0700

    almost same as multiplication, to be fixed though

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: 1st param
+ * @argv: second param
+ *
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i, resul = 0;
+
+	if (argc == 1)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i <= argc - 1; i++)
+		{
+			resul = resul + atoi(argv[i]);
+		}
+		printf("%d\n", resul);
+	}
+	return (0);
+}
