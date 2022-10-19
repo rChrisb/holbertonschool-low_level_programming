884d25070981c3f7ff0fdb0736a57ed868b37a8f
rChrisb <5767@holbertonstudents.com>
2022-10-19 04:04:49 -0700

    a program that adds positive numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,20 +13,39 @@ int main(int argc, char *argv[])
 {
 	int i, resul = 0;
 
+	char *s;
+
 	if (argc < 1)
 	{
 		return (0);
 	}
+	
+	i = 1;
+
+	while (i < argc)
+	{
+		int k = 0;
+
+		s = argv[k];
+
+		while (s[k] != '\0')
+		{
+			if (s[k] < '0' || s[k] > '9')
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+			k++;
+		}
+		i++;
+	}
+
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
-		{
-			printf("%s\n", "Error");
-			return (1);
-		}
 		resul = resul + atoi(argv[i]);
 	}
 	printf("%d\n", resul);
 
 	return (0);
 }
+
