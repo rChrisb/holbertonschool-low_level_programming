27b0686275be9acfeddd73e6767d150391f173f6
rChrisb <5767@holbertonstudents.com>
2022-10-23 09:30:40 -0700

    Create new directory, its README file, its main.h file containing functions prototypes, copy the _putchar file

---
+++ more_malloc_free/README.md
@@ -0,0 +1,2 @@
+This is the second project about memory allocation, it will allow a better understanding on How to use the exit function
+> What are the functions calloc and realloc from the standard library and how to use them
---
+++ more_malloc_free/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ *
+ * Structs, enums and unions defintions
+ * Typedefs
+ * Function prototypes
+ *
+ */
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+int _putchar(char c)
+
+
+#endif
