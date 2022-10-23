ce530273b256776a6a53bf796fabc759c3ef46de
rChrisb <5767@holbertonstudents.com>
2022-10-23 04:11:09 -0700

    Creation of main.h file with its prototypes in it

---
+++ malloc_free/main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ *
+ * Structs, enums and unions defintions
+ * Typedefs
+ * Function prototypes
+ *
+ */
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+
+
+
+
+
+
+
+
+
+
+#endif
