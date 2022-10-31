88948436216ea97be2891c07f1b5d04add79da4f
rChrisb <5767@holbertonstudents.com>
2022-10-31 02:03:52 -0700

    Write a function that creates a new dog, having to store a copy of name and owner

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+#include <string.h>
+
+/**
+ * new_dog - new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: name, age, owner
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+	char *n;
+	char *o;
+
+	new = malloc(sizeof(dog_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	n = malloc(sizeof(char) * strlen(name));
+	if (n == NULL)
+		return (NULL);
+
+	o = malloc(sizeof(char) * strlen(owner));
+	if (o == NULL)
+		return (NULL);
+
+	new->name = memcpy(n, name, strlen(name) + 1);
+	new->owner = memcpy(o, owner, strlen(owner) + 1);
+	new->age = age;
+	return (new);
+}
