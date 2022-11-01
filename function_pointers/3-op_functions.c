989d7327e52492646619c92e6d7f10b1a83f6a9e
rChrisb <5767@holbertonstudents.com>
2022-11-01 12:26:38 -0700

    Edit description

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -30,7 +30,7 @@ int op_sub(int a, int b)
  * op_mul - multiplies 2 int
  * @a: first int
  * @b: second int
- * Return: multiplication of a by b
+ * Return: multiplication between a and b
  */
 int op_mul(int a, int b)
 {
