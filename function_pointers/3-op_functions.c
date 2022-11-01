43a7c0c0013f87dd100059d3736bb04605138a7f
rChrisb <5767@holbertonstudents.com>
2022-11-01 02:05:38 -0700

    file containing the functions for addition, substraction, multiplication, division and modulo

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include "calc.h"
+
+/**
+ * op_add - adds 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - subs 2 int
+ * @a: first int
+ * @b: second int
+ * Return: substraction
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - multiplies 2 int
+ * @a: first int
+ * @b: second int
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - divides 2 int
+ * @a: first int
+ * @b: second int
+ * Return: division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - returns modulo of an int by another
+ * @a: first int
+ * @b:second int
+ * Return: modulo of a by b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
