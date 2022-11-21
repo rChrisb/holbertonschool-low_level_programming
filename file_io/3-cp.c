35cd23e5d8d320f73aaf65b3db6ebba8171c7f6f
rChrisb <5767@holbertonstudents.com>
2022-11-21 02:54:12 -0800

    program that copies the content of a file to another file

---
+++ file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * main - program that copies the content of a file to another file
+ *
+ * @argc: number of arguments
+ * @argv: values of arguments
+ *
+ * Return: 0
+ */
+
+int main(ac, **av)
+{
+	FILE *source, *dest;
+	char buf[1024];
+	
+	int fd = open(dest, O_TRUNC | O_CREAT | O_RDWR, 664);
+
+
+
+
+	
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	if (source == NULL)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (dest == NULL)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+
+
+
+	if (close(fd) != 0)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+
+
+	return (0);
+}
