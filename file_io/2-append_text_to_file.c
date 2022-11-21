e39dec963674ae3a74959d39551fdd855b1633cb
rChrisb <5767@holbertonstudents.com>
2022-11-21 01:45:38 -0800

    function that appends text at the end of a file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,9 +14,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	FILE *append;
 	append = fopen(filename, "a");
 
+	if (filename == 0 || append == NULL)
+		return (-1);
 
+	if (text_content == NULL)
+		return (1);
 
-	fprintf(filename, "%s", text_content);
+	fprintf(append, "%s", text_content);
 
 	return (1);
 }
