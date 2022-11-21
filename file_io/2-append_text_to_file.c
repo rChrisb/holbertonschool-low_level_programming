a842e73a29ecc24c5c08e19b1720562efccd2a6e
rChrisb <5767@holbertonstudents.com>
2022-11-21 01:37:43 -0800

    test

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ *
+ * @filename: pointer to a file
+ * @text_content: content of file
+ *
+ * Return: 1 or -1(failure)
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	FILE *append;
+	append = fopen(filename, "a");
+
+
+
+	fprintf(filename, "%s", text_content);
+
+	return (1);
+}
