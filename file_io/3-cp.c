0ed4d618b3fd97dd03b20949841df02129c1c3bc
rChrisb <5767@holbertonstudents.com>
2022-11-21 07:49:32 -0800

    cp function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,12 +40,13 @@ int main(int ac, char **av)
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
-		if (x == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
 	}
+	if (x == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	
 
 
 	source = close(source);
