f44441f060cae2ea39230e3fa1ef46683afef0e3
rChrisb <5767@holbertonstudents.com>
2022-11-21 07:44:37 -0800

    cp function

---
+++ file_io/3-cp.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * main - program that copies the content of a file to another file
+ *
+ * @ac: number of arguments
+ * @av: values of arguments
+ *
+ * Return: 0
+ */
+
+int main(int ac, char **av)
+{
+	int source, dest;
+	char buf[1024];
+	long int x;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	source = open(av[1], O_RDONLY);
+	if (source == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	dest = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (dest == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	while ((x = read(source, buf, 1024)) > 0)
+	{
+		x = write(dest, buf, x);
+		if (x == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+		if (x == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+	}
+
+
+	source = close(source);
+	if (source == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", dest);
+		exit(100);
+	}
+	dest = close(dest);
+	if (dest == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", dest);
+		exit(100);
+	}
+	return (0);
+
+}
