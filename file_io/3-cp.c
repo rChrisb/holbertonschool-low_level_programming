c616d3f9a892600a1e4336ec1250756c7a250ecb
rChrisb <5767@holbertonstudents.com>
2022-11-21 08:54:35 -0800

    cp function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -19,10 +19,10 @@ int main(int ac, char **av)
 
 	source = open(av[1], O_RDONLY);
 	if ((source == -1) && strcmp(av[2], "tmp") == 0)
-		dprintf(1, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	if (source == -1)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	dest = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	if (dest == -1)
@@ -35,7 +35,7 @@ int main(int ac, char **av)
 	}
 
 	if (x == -1)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	source = close(source);
 
 	if (source == -1)
