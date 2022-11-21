70f491e0de4c457881b9d29e928da82c927e45b1
rChrisb <5767@holbertonstudents.com>
2022-11-21 01:22:54 -0800

    function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,8 +18,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (filename == NULL)
 		return (-1);
-
-
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		return (1);
+	}
 	write(fd, text_content, strlen(text_content));
 
 	return (1);
