2f940eb604af0e1fa6c722fae4abaf37e448b836
rChrisb <5767@holbertonstudents.com>
2022-11-23 02:27:16 -0800

    Write a function that inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: first node
@@ -8,18 +7,13 @@
  *
  * Return:  the address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *temp;
 	unsigned int i;
-
-
 	new = malloc(sizeof(struct dlistint_s));
-
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
@@ -28,26 +22,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->next = *h;
 		*h = new;
 	}
-	temp = *h;
-
-	i = 0;
-	while (i < idx - 1)
+	else
 	{
-		if (temp == NULL)
-			return (NULL);
-		i++;
-		temp = temp->next;
+		temp = *h;
+
+		i = 0;
+		while (i < idx - 1)
+		{
+			if (temp == NULL)
+				return (NULL);
+			i++;
+			temp = temp->next;
+		}
+
+		if (temp != NULL)
+		{
+			new->next = temp->next;
+			if (temp->next != NULL)
+			{
+				temp->next->prev = new;
+				new->next->prev = new;
+			}
+			temp->next = new;
+			new->prev = temp;
+		}
 	}
-	if (temp->next == NULL)
-		return (add_dnodeint_end(h, n));
-	if (temp->next != NULL)
-	{
-		new->next = temp->next;
-		temp->next->prev = new;
-		new->next->prev = new;
-		temp->next = new;
-		new->prev = temp;
-	}	
-
 	return (new);
 }
