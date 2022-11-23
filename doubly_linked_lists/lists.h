80966faaeac7c62aa4d3a673916ea0d635914248
rChrisb <5767@holbertonstudents.com>
2022-11-23 02:48:05 -0800

    New prototype

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -29,7 +29,7 @@ int sum_dlistint(dlistint_t *head);
 void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 
 #endif
