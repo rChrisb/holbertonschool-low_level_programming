ead514d4c5533c0f59ac777cc844f5899e23a5e7
rChrisb <5767@holbertonstudents.com>
2022-11-23 00:39:31 -0800

    Write a function that inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
  * Return:  the address of the new node, or NULL if it failed
  */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *temp;
 	unsigned int i;
