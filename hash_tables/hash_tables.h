3b9e6257c7b9d87077a227dd2cae85d44a6cacff
rChrisb <5767@holbertonstudents.com>
2022-11-26 02:26:13 -0800

    Header file

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -44,7 +44,7 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const char *str);
 unsigned long int key_index(const char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-
+hash_node_t *ht_k_v(const char *key, const char *value);
 
 
 #endif
