1e9ba03c9ecfa0131b7f029527f1dc94fbe6ebb2
rChrisb <5767@holbertonstudents.com>
2022-11-26 01:01:19 -0800

    Write a function that creates a hash table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the hashtable
+ *
+ * Return: the crezted table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+}
