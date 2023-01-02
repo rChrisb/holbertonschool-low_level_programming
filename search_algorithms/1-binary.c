a7bc915a1b69cada6b3723061efb16cbadc4d620
rChrisb <5767@holbertonstudents.com>
2023-01-02 12:54:44 +0100

    Write a function that searches for a value in a sorted array of integers using the Binary search algorithm

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -13,13 +13,15 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t middle, start = 0, end = size - 1;
 
+	if (size == 0)
+		return (-1);
 	while (end - start > 1)
 	{
 		middle = (end + start) / 2;
 		if (value > array[middle])
 		{
 			print_array(array, start, end);
-			start = middle +1;
+			start = middle + 1;
 		}
 		else
 		{
@@ -41,7 +43,6 @@ int binary_search(int *array, size_t size, int value)
 	else
 	{
 			print_array(array, start, end);
-		
 			printf("Searching in array: %d\n", array[end]);
 		return (-1);
 	}
@@ -60,11 +61,11 @@ void print_array(int *array, size_t start, size_t end)
 	size_t k;
 
 	printf("Searching in array: ");
-		for (k = start; k <= end; ++k)
-			{
-				if (k != start)
-					printf(", ");
-				printf("%d", array[k]);
-			}
-			printf("\n");
+	for (k = start; k <= end; ++k)
+	{
+		if (k != start)
+			printf(", ");
+		printf("%d", array[k]);
+	}
+	printf("\n");
 }
